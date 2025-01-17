aed8cef285dca15d72354977b80ae487aca3a4eb
Kaddyp <kaddyp2805@gmail.com>
2025-01-17 15:05:12 +0000

    Make application running.

--- src/Continents.tsx
+++ src/Continents.tsx
@@ -2,34 +2,42 @@
 import styled from 'styled-components';
 import { useQuery, gql } from '@apollo/client';
 
-type ContinenntsComponent = () => {};
+type ContinenntsComponent = () => JSX.Element;
 
 const Continents: ContinenntsComponent = () => {
   const CONTINENTS = gql`
     query Continents {
       continents {
-        name
+          code,
+          name,
+          countries {
+            capital
+            currency
+          }
       }
     }
   `;
 
-  interface ContinentsQery {
+  interface ContinentsQuery {
     continents: Array<{
       name: string;
     }>;
   }
 
-  const { loading, error, data } = useQuery<ContinentsQery>(CONTINENTS);
+  const { loading, error, data } = useQuery<ContinentsQuery>(CONTINENTS);
 
   const bold = 700;
 
-  const isEurope = (c) => (c !== 'Europe' ? false : true);
+  //const isEurope = (c) => (c !== 'Europe' ? false : true);
+  const isEurope = (c: string) => c === 'Europe';
 
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
   return (
     <div>
-      <h3 class={`font-${bold}`}>Continents:</h3>
+      <h3 className={`font-${bold}`}>Continents:</h3>
       {data?.continents.map(({ name }) => (
-        <div className={isEurope(name) && 'text-red-800'}>{name}</div>
+        <div className={isEurope(name) ? 'text-red-800' : ''}>{name}</div>
       ))}
     </div>
   );
