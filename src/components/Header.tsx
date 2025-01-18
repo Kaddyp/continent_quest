d2441aab0fc5c83ced267da8e611fc6f1cd65abd
Kaddyp <kaddyp2805@gmail.com>
2025-01-18 22:05:09 +0000

    Logo added.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import Logo from '../logo.png';
 const Header: React.FC = () => {
   return (
     <header>
-      <nav className="px-4 py-2">       
-        <h1 className="text-2xl font-bold">Continent Quest</h1>
+      <nav className="px-4 py-2">
+        <div className="flex items-center">
+          <a href="/">            
+            <img src={Logo} alt='logo' width={'36px'} height={'36px'} className=""/> 
+          </a>
+          <div className="hidden md:block">
+            <div className="ml-5 flex items-baseline space-x-4">
+              <h1 className="text-2xl font-bold">Continent Quest</h1>
+            </div>
+          </div>
+        </div>
       </nav>
     </header>
   );
