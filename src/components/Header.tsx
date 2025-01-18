610e5f22aef9e949740f8856a056c4243fa72562
Kaddyp <kaddyp2805@gmail.com>
2025-01-18 11:54:20 +0000

    Reusable header component created.

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+const Header: React.FC = () => {
+  return (
+    <header>
+      <nav className="px-4 py-2">       
+        <h1 className="text-2xl font-bold">Continent Quest</h1>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,11 @@
 import { Outlet, Link } from "react-router-dom";
+import Header from "./Header";
 
 const Layout: React.FC = () => {
   return (
     <div className="bg-white">
-      <div className="mx-auto max-w-7xl py-6 sm:px-6 sm:py-6 lg:px-8">
-        <nav className="px-4 py-2">       
-          <h1 className="text-2xl font-bold">Continent Quest</h1>
-        </nav>
+      <div className="mx-auto max-w-7xl py-6 sm:px-6 sm:py-6 lg:px-8">        
+        <Header />
         <Outlet />
       </div>
     </div>
